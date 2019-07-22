245289e7ac9ba0a3aeb408a1a5ff6b7bf7f0a08f
Nerajno <Nerajno@gmail.com>
2019-07-22 14:20:25 -0400

    added fetch function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 // It might be a good idea to add event listener to make sure this file 
 // only runs after the DOM has finshed loading. 
+
+//Step one
+let quotes_url = "http://localhost:3000/quotes?_embed=likes"
+
+document.addEventListener('DOMContentLoaded', setUpPage)
+
+function setUpPage(){
+    fetchQuotes()
+    makeQuoteCard()
+   // document.addEventListener("click", handleAddPokemon
+
+}
+function fetchQuotes(){
+    fetch(quotes_url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+function makeQuoteCard() {
+    `<li class='quote-card'>
+    <blockquote class="blockquote">
+      <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer">Someone famous</footer>
+      <br>
+      <button class='btn-success'>Likes: <span>0</span></button>
+      <button class='btn-danger'>Delete</button>
+    </blockquote>
+  </li>`
+    
+}
